ec7c9f91710d0cebbb1b5d9cae6bb6cf470e7202
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-04-11 00:05:14 +0200

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([, userResponse]) => {
-      console.log(`${userResponse.body.firstName} ${userResponse.body.lastName}`);
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.body.firstName} ${userResponse.body.lastName}`);
     })
-    .catch(() => {
-      console.error('Signup system offline');
-    });
+    .catch(() => console.error('Signup system offline'));
 }
