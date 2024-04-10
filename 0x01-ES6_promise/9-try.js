62315e67bb7916d0c35e7ff37798405505b2ceb9
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-04-11 00:50:53 +0200

    100-await.js

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+  const queue = ['Guardrail was processed'];
+  
+  try {
+    const value = mathFunction();
+    queue.unshift(value);
+  } catch (err) {
+    queue.unshift(err.toString());
+  }
+
+  return queue;
+}
