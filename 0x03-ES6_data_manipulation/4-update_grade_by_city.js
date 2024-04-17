ccb579037b9cd5efc216aec75d4cac3a2e1d521a
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-04-17 02:15:51 +0200

    4-update_grade_by_city.js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => ({
+      ...student,
+      grade: (newGrades.find((grade) => grade.studentId === student.id) || { grade: 'N/A' }).grade,
+    }));
+}
+
+export default updateStudentGradeByCity;
