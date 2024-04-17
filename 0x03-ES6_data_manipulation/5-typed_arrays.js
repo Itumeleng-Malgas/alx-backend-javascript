fc4560e8ecaed7cfdb511624e8c1ae00318a8937
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-04-17 02:30:35 +0200

    rectified

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,6 @@
 const createInt8TypedArray = (length, position, value) => {
   const buffer = new ArrayBuffer(length);
+  if (position > int8Array.length) throw new Error('Position outside range');
   new Int8Array(buffer)[position] = value;
   return new DataView(buffer);
 };
