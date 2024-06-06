eb9615e8db82f415a58040bf155377d27ce7ea6d
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-06-06 20:59:01 +0200

    Added few Edge cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,17 +4,21 @@ const calculateNumber = require("./0-calcul");
 
 describe("calculateNumber()", function() {
 
-    it('checking expected bahaviour', function() {
+    it('checking no rounding off', function() {
       const result = calculateNumber(1, 1);
       assert.strictEqual(result, 2);
     });
-    it('checking rounding off', function() {
-      const result = calculateNumber(1.1, 2.5);
-      assert.strictEqual(result, 4);
+    it('checking rounding off up', function() {
+      const result = calculateNumber(1.7, 2.5);
+      assert.strictEqual(result, 5);
     });
-    it('checking negative input', function() {
-      const result = calculateNumber(-1.1, -2.0);
-      assert.strictEqual(result, -3);
+    it('checking rounding negative', function() {
+      const result = calculateNumber(-1.5, -2.8);
+      assert.strictEqual(result, -4);
+    });
+    it('checing rouding off down', function() {
+      const result = calculateNumber(1.4, 2.2);
+      assert.strictEqual(result, 3);
     });
     it('checking zeros as inputs', function() {
       const result = calculateNumber(0, 0);
