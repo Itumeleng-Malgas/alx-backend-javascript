4bc14299ab91270aa50d5f29ffede4fd3652c80d
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-06-06 23:27:04 +0200

    4-payment.

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,16 +1,20 @@
 const {describe, it} = require("mocha");
-const sinon = require("sinon");
-const sendPaymentRequestToApi = require("./4-payment");
-const Utils = require("./utils");
 const assert = require("assert");
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
 
-describe("sendPaymentRequestToApi", function() {
-    it("check that Utils.calculateNumber is stubbed", function() {
-	const spy = sinon.spy(console, "log");
-	const stub = sinon.stub(Utils, "calculateNumber").returns(10);
-	sendPaymentRequestToApi(100, 20);
+describe('sendPaymentRequestToApi', function() {
+    it('should use a stub for Utils.calculateNumber and verify the correct console.log message', function() {
+        const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+        const consoleLogSpy = sinon.spy(console, 'log');
 
-	assert(spy.withArgs("The total is: 10").calledOnce);
-	assert(stub.withArgs("SUM", 100, 20).calledOnce);
+        sendPaymentRequestToApi(100, 20);
+
+	    assert(consoleLogSpy.withArgs("The total is: 10").calledOnce);
+	    assert(calculateNumberStub.withArgs("SUM", 100, 20).calledOnce);
+        
+        calculateNumberStub.restore();
+        consoleLogSpy.restore();
     });
 });
